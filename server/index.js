d0b0a34a7cf34ca01bb91d1e9c35bc7c83b3727a
Nicolas Matthew Vinson Wauquiez <nmvinson@gmail.com>
2017-08-13 11:34:40 -0600

    deployd needs es5 so rewrite index.js

--- server/index.js
+++ server/index.js
@@ -1,21 +1,28 @@
 // require deployd
 var deployd = require('deployd');
 
-// configure database etc. 
-var [type, username, login, port_dbname] = process.env.MONGODB_URI;
-var [port, db_name] = port_dbname.split('/');
-var [password, host] = login.split('@');
+// configure database etc.
+
+// TODO: ES6 update -> watch deployd dependency on es5!
+// let [type, username, login, port_dbname] = process.env.MONGODB_URI.split(':');
+// let [port, db_name] = port_dbname.split('/');
+// let [password, host] = login.split('@');
+var uri = process.env.MONGODB_URI.split(':');
+var username = uri[1];
+var login = uri[2];
+var port_dbname = uri[3].split('/');
+var password_host = login.split('@');
 
 var server = deployd({
   port: process.env.PORT || 5000,
   env: 'production',
   db: {
-    host,
-    port: +port,
-    name: db_name,
+    host: password_host[1],
+    port: +port_dbname[0],
+    name: port_db_name[1],
     credentials: {
       username,
-      password
+      password: password_host[0]
     }
   }
 });
